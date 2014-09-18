228e3b915240eef557a54d7392fb1ba2e6ce9959
Alexey Belousov <frgt@yandex-team.ru>
2014-09-18 17:21:19 +0600

    tests refactoring

--- tests/crawler.js
+++ tests/crawler.js
@@ -3,8 +3,8 @@
 var grabby = require('../lib/crawler'),
     fs = require('fs'),
     nock = require('nock'),
-    assert = require('assert'),
-    vow = require('vow');
+    vow = require('vow'),
+    expect = require('chai').expect;
 
 nock.disableNetConnect();
 
@@ -20,42 +20,44 @@ var responses = {
 describe('grabby', function() {
     describe('#requestHtml', function () {
         it('can request simple html pages', function() {
-            nock('http://ya.ru')
+            var scope = nock('http://ya.ru')
                 .get('/')
                 .reply(200, responses['ya.ru-plain'], {});
 
             var request = {url: 'http://ya.ru/'};
 
             return grabby.requestHtml(request).then(function (html) {
-                assert.equal(true, (/<title>Яндекс<\/title>/gi).test(html));
+                expect((/<title>Яндекс<\/title>/gi).test(html)).to.equal(true);
+
+                // scope done
+                expect(scope.isDone()).to.equal(true);
             });
         });
 
         it('can request html pages encoded with gzip', function() {
-            nock('http://ya.ru')
+            var scope = nock('http://ya.ru')
                 .get('/')
                 .reply(200, responses['ya.ru-gzip'], {'Content-Encoding': 'gzip'});
 
             var request = {url: 'http://ya.ru/', headers: {'Accept-Encoding': 'gzip'}};
 
             return grabby.requestHtml(request).then(function (html) {
-                assert.equal(true, (/<title>Яндекс<\/title>/gi).test(html));
+                expect((/<title>Яндекс<\/title>/gi).test(html)).to.equal(true);
+
+                // scope done
+                expect(scope.isDone()).to.equal(true);
             });
         });
 
         it('can make few attempts if meet 409', function () {
-            nock('http://ya.ru')
+            var scope = nock('http://ya.ru')
                 .get('/')
                 .times(1)
                 .reply(409, 'too often')
 
                 .get('/')
                 .times(1)
-                .reply(200, responses['ya.ru-plain'], {})
-
-                .get('/')
-                .times(1)
-                .reply(409, 'too often');
+                .reply(200, responses['ya.ru-plain'], {});
 
             var request = {
                 url: 'http://ya.ru/',
@@ -67,19 +69,18 @@ describe('grabby', function() {
             };
 
             return grabby.requestHtml(request).then(function (html) {
-                assert.equal(true, (/<title>Яндекс<\/title>/gi).test(html));
+                expect((/<title>Яндекс<\/title>/gi).test(html)).to.equal(true);
+
+                // scope done
+                expect(scope.isDone()).to.equal(true);
             });
         });
 
         it('make only 1 attempt if limit set to 1', function () {
-            nock('http://ya.ru')
+            var scope = nock('http://ya.ru')
                 .get('/')
                 .times(1)
-                .reply(409, 'too often')
-
-                .get('/')
-                .times(1)
-                .reply(200, responses['ya.ru-plain'], {});
+                .reply(409, 'too often');
 
             var request = {
                 url: 'http://ya.ru/',
@@ -95,7 +96,29 @@ describe('grabby', function() {
                     throw new Error();
                 },
                 function () {
-                    return 1;
+                    // scope done
+                    expect(scope.isDone()).to.equal(true);
+                }
+            );
+        });
+
+        it('failed on timeout', function () {
+            var scope = nock('http://ya.ru')
+                .get('/')
+                .delayConnection(2000)
+                .reply(200, responses['ya.ru-plain'], {});
+
+            var request = {url: 'http://ya.ru/', timeout: 1};
+
+            return grabby.requestHtml(request).then(
+                function () {
+                    throw new Error();
+                },
+                function (err) {
+                    expect(err.message).to.equal('ETIMEDOUT');
+
+                    // scope done
+                    expect(scope.isDone()).to.equal(true);
                 }
             );
         });
